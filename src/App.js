18cf4a7b1a1921142a0eb5109a4c59cabb62f26c
Kevin Lau <kevlau82@gmail.com>
2023-05-14 16:52:12 -0400

    refactored and formatted code

--- src/App.js
+++ src/App.js
@@ -1,110 +1,111 @@
-import Axios from 'axios'
-import {useState, useEffect} from 'react'
-import './App.css';
-import Coin from './components/Coin'
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Coin from "./components/Coin";
 
 function App() {
-
   useEffect(() => {
-    document.title = "Crypto Prices"
+    document.title = "Crypto Prices";
   }, []);
 
-    //coin data
-    const [listOfCoins, setListOfCoins] = useState([])
-    //search state
-    const [search, setSearch] = useState('')
-    //dropdown selected state
-    const [selected, setSelected] = useState('')
-   
+  //coin data
+  const [listOfCoins, setListOfCoins] = useState([]);
+  //search state
+  const [search, setSearch] = useState("");
+  //dropdown selected state
+  const [selected, setSelected] = useState("");
 
-      //initalize
-     let preFilter = listOfCoins    
+  //initalize
+  let preFilter = listOfCoins;
 
-    //sort depending on what the selected value is (dropdown)
-     if (selected === 'PriceLH') {
-     preFilter = listOfCoins.sort((a,b) => {
-      return a.price-b.price
-    }) 
-    }
-    else if(selected ==='PriceHL'){
-      preFilter = listOfCoins.sort((a,b) => {
-        return b.price-a.price
-      })
-    }
-    else if (selected === 'Alphabetical'){
+  //sort depending on what the selected value is (dropdown)
+  if (selected === "PriceLH") {
+    preFilter = listOfCoins.sort((a, b) => {
+      return a.price - b.price;
+    });
+  } else if (selected === "PriceHL") {
+    preFilter = listOfCoins.sort((a, b) => {
+      return b.price - a.price;
+    });
+  } else if (selected === "Alphabetical") {
+    preFilter = listOfCoins.sort((a, b) => {
+      return a.name < b.name ? -1 : 1;
+    });
+  } else if (selected === "Popularity") {
+    preFilter = listOfCoins.sort((a, b) => {
+      return a.rank - b.rank;
+    });
+  }
 
-      preFilter = listOfCoins.sort((a,b) => {
-        return a.name < b.name ? -1 : 1
-      })
-    }
+  const filtered = preFilter.filter((coin) => {
+    //filter if search lowercase with spaces removed is contained in space removed coin name
+    return coin.name
+      .toLowerCase()
+      .replace(/ /g, "")
+      .includes(search.toLowerCase().replace(/ /g, ""));
+  });
 
-    else if (selected === 'Popularity') {
-      preFilter = listOfCoins.sort((a,b)=> {
-        return a.rank - b.rank
-      })
-      
-    }
-    
-     const filtered = preFilter.filter((coin) => {
-       //filter if search lowercase with spaces removed is contained in space removed coin name
-      return (coin.name.toLowerCase()).replace(/ /g, '').includes(search.toLowerCase().replace(/ /g, ''));
- 
-     })
+  //useEffects run immedatiely when the page rerenders
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
+      (response) => setListOfCoins(response.data.coins)
+    );
+  }, []);
 
-
-
-    //useEffects run immedatiely when the page rerenders
-    useEffect(() => {
-        Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
-        .then((response) => setListOfCoins(response.data.coins))
-    },[])
-
-      
-    console.log(listOfCoins)
-
+  console.log(listOfCoins);
 
   return (
     <>
-     <div className="App">
-      <div className="header">
-      <h1>Crypto Price Tracker</h1>
-        {/* search bar */}
-      <input className="searchBar" type="text" placeholder="Search for a crypto..." onChange={(event) => {
-        setSearch(event.target.value)
-      }}></input>
-
-</div>
-{/* dropdown select */}
-<section id="header-container">
-  <select onChange={(event) => {
-    setSelected(event.target.value)
-  }}>
-  <option value="Select" disabled >Sort by:</option>
-    <option value="Popularity">Popularity</option>
-    <option value="PriceLH">Price: Low to High</option>
-    <option value="PriceHL">Price: High to Low</option>
-    <option value="Alphabetical">Alphabetical</option>
-  </select>
-</section>
-  {/* grid -> output msg if theres no stuff in the array*/}
-      <div className="display">{filtered.length > 0 ?  filtered.map((coin)=> {
-        return <Coin name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol} /> 
-      }): `No cryptos matching '${search}'`}
-      
+      <div className="App">
+        <div className="header">
+          <h1>Crypto Price Tracker</h1>
+          {/* search bar */}
+          <input
+            className="searchBar"
+            type="text"
+            placeholder="Search for a crypto..."
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          ></input>
+        </div>
+        {/* dropdown select */}
+        <section id="header-container">
+          <select
+            onChange={(event) => {
+              setSelected(event.target.value);
+            }}
+          >
+            <option value="Select" disabled>
+              Sort by:
+            </option>
+            <option value="Popularity">Popularity</option>
+            <option value="PriceLH">Price: Low to High</option>
+            <option value="PriceHL">Price: High to Low</option>
+            <option value="Alphabetical">Alphabetical</option>
+          </select>
+        </section>
+        {/* grid -> output msg if theres no stuff in the array*/}
+        <div className="display">
+          {filtered.length > 0
+            ? filtered.map((coin) => {
+                return (
+                  <Coin
+                    name={coin.name}
+                    icon={coin.icon}
+                    price={coin.price}
+                    symbol={coin.symbol}
+                  />
+                );
+              })
+            : `No cryptos matching '${search}'`}
+        </div>
       </div>
-
-
-    </div>
-    <footer>
-      Developed by Kevin Lau 
-    </footer>
-    
-
+      <footer>Developed by Kevin Lau</footer>
     </>
-    
   );
 }
 
 export default App;
 
-//AXIOS 
+//AXIOS
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,17 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function Coin({name, icon, price, symbol}) {
+function Coin({ name, icon, price, symbol }) {
   return (
-    <div className='coin'>
-    <h1>{name}</h1>
-    <img src={icon} />
-    <h3 id="price">$ {price.toFixed(2) == 0.00 ? 0.01: price.toFixed(2)}</h3>
-    <h3 id="symbol"> {symbol} </h3>
-
+    <div className="coin">
+      <h1>{name}</h1>
+      <img src={icon} />
+      <h3 id="price">$ {price.toFixed(2) == 0.0 ? 0.01 : price.toFixed(2)}</h3>
+      <h3 id="symbol"> {symbol} </h3>
     </div>
-  )
+  );
 }
 
-
-
-export default Coin
+export default Coin;
